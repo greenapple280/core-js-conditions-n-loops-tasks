2c917247ab1a70be0f8a6d4758c66cb972ee8e03
Tanja Kiseleva <lessy_92@mail.ru>
2025-01-13 19:27:58 +0100

    fix: isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -203,8 +203,15 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = `${num}`;
+  for (let i = 0; i <= str.length - 1; i += 1) {
+    if (str[i] === `${digit}`) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 /**
@@ -220,8 +227,11 @@ function isContainNumber(/* num, digit */) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 1; i < arr.length; i += 1) {
+    if (condition) {
+    }
+  }
 }
 
 /**
